bc8528c3f8395484c5d6abb1ac30c042dc980685
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-01-16 23:00:17 +0200

    initial sitemap

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  env: {
-    siteName: 'cs-boost.lt',
-    domain: 'https://cs-boost.lt',
-    defaultTitle: 'CS 1.6 | Counter-Strike 1.6 Servers',
-    defaultDescription: 'CS 1.6 Servers, Counter-Strike server list, CS 1.6 BOOST',
-    googleAnalyticsKey: 'UA-125851250-1'
-  },
+  env: require('./site.config'),
   images: {
     disableStaticImages: true
   }
---
+++ site.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteName: 'cs-boost.lt',
+  domain: 'https://cs-boost.lt',
+  defaultTitle: 'CS 1.6 | Counter-Strike 1.6 Servers',
+  defaultDescription: 'CS 1.6 Servers, Counter-Strike server list, CS 1.6 BOOST',
+  googleAnalyticsKey: 'UA-125851250-1'
+};
