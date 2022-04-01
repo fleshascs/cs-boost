2f78f4d2dc02b4db22c0530b06352087277fe542
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-04-01 13:04:56 +0300

    description updates

--- site.config.js
+++ site.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteName: 'cs-boost.lt',
   domain: 'https://cs-boost.lt',
-  defaultTitle: 'Counter-Strike 1.6 Server List - TOP CS 1.6 servers',
+  defaultTitle: 'CS-BOOST.LT - Buy Counter-Strike 1.6 boost services',
+  // defaultDescription:
+  //   'A ranked Counter-Strike server list of Worlds TOP CS 1.6 Servers, containing a different CS mods: CSDM, CS CSGO 1.6 Mod, Classic, Knife mod, Zombie, Umbrella Swarm, Surf, Deathrun, Bhop, Hide&Seek (HNS)m GunGame',
   defaultDescription:
-    'A ranked Counter-Strike server list of Worlds TOP CS 1.6 Servers, containing a different CS mods: CSDM, CS CSGO 1.6 Mod, Classic, Knife mod, Zombie, Umbrella Swarm, Surf, Deathrun, Bhop, Hide&Seek (HNS)m GunGame',
+    'Counter-Strike 1.6 boost is a service for publishing and advertising your Counter-Strike 1.6 server. A player launches Counter-Strike and looks for a server. After clicking on "Find servers," the player is presented with a list of available servers.',
   googleAnalyticsKey: 'UA-125851250-1'
 };
