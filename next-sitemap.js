6d04e26bf4a4f62144b3ed01820d2d6504636c58
fleshascs <35168446+fleshascs@users.noreply.github.com>
2022-07-04 22:33:43 +0300

    changes for payment complete page

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
   siteUrl: require('./site.config').domain,
   generateRobotsTxt: true,
-  exclude: ['/server-info', '/logo']
+  exclude: ['/server-info', '/logo', '/payment-complete']
 };
--- pages/payment-complete.tsx
+++ pages/payment-complete.tsx
@@ -1,8 +1,33 @@
 import { useFetchServers } from '../components/utils';
 import Top from '../components/Top';
+import { Spinner } from '../components/Spinner';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const useDummyCountdown = (seconds) => {
+  const [time, setTime] = useState<number>(seconds);
+
+  useEffect(() => {
+    const task = setInterval(() => {
+      setTime((t) => t - 1);
+    }, 1000);
+    return () => {
+      clearInterval(task);
+    };
+  }, []);
+
+  return time;
+};
 
 export default function Contact() {
   const { total } = useFetchServers();
+  const router = useRouter();
+
+  const seconds = useDummyCountdown(60);
+  useEffect(() => {
+    if (seconds === 40) router.push('/');
+  }, [seconds]);
+
   return (
     <>
       <Top
@@ -12,9 +37,15 @@ export default function Contact() {
         percentage={total.percentage}
       />
 
-      <div className='pb-24'>
-        <h2 className='py-2 text-md font-medium'>Success</h2>
-        The payment has been completed,{' '}
+      <div className='pb-24 flex flex-col items-center space-y-4'>
+        <h2 className='py-2 text-md font-medium text-green-500 text-3xl'>Payment completed</h2>
+
+        <Spinner />
+
+        <div className='py-2 text-md font-medium'>{seconds}s.</div>
+
+        <div className='py-2 text-md font-medium text-4xl'>Verification in progress...</div>
+
         <a href='/' className='font-semibold text-red-700'>
           Go back to the main page
         </a>
